7ecd7fa82d6bd4018655b59769cdcd0649707be0
andbret <and.bretnall@gmail.com>
2020-09-03 18:26:35 -0400

    fully functional

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-// Assignment Code
-// var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// var specialArray = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?"];
+function generatePassword() {
+  var length = prompt("How long do you want your password to be? it must be between 8 and 128 characters.");
+  if (length !== null && length !== "" && length > 7 && length < 129) {
 
-
-
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function myFunction() {
-  var txt;
-  var length = prompt("How long do you want your password to be? it must be between 8 and 128 char");
-  if (length == null || length == "") {
-    alert("User cancelled the prompt.")
-  } else {
     console.log(length)
     var lowerCase = confirm("Do you want to add lower case letters?");
     if (lowerCase == true) {
@@ -55,35 +31,42 @@ function myFunction() {
 
     var special = confirm("Do you want to add special characters?");
     if (special == true) {
-      var specialArray = ["+", "-", "&", "!", "^", "~", "*", "?"];
+      var specialArray = ["+", "-", "&", "!", "^", "~", "*", "?", "#", "$", "%", "&", "(", ")", ":", ";", "<", ">", "@"];
     }
     else {
       var specialArray = [];
     }
+    console.log(special);
+    console.log(upperCase);
+    console.log(lowerCase);
+    console.log(numeric);
+    if (special == false && upperCase == false && lowerCase == false && numeric == false){
+      alert("You must include at least one option.")
+    }
+    else{
     var passwordOptions = lowerCaseArray.concat(upperCaseArray, numericArray, specialArray);
 
     console.log(passwordOptions);
 
     var passwordCharacters = []
     for (let i = 0; i < length; i++) {
-      const characterCode = passwordOptions[Math.floor(Math.random() * passwordOptions.length)]
-      console.log(characterCode);
-      passwordCharacters.push(characterCode)
+      const individualCharacter = passwordOptions[Math.floor(Math.random() * passwordOptions.length)]
+      console.log(individualCharacter);
+      passwordCharacters.push(individualCharacter)
       console.log(passwordCharacters);
+      console.log(passwordCharacters.join(''));
+ 
+      console.log(password);
+      document.getElementById("password").innerHTML = passwordCharacters.join('');
     }
-    // console.log(passwordCharacters);
-    // return passwordCharacters.join('')
-   
     
-
+    }
+  } else {
+    alert("This is not a valid number.")
   }
 
-  // return passwordCharacters.join('')
+
+
+
   
 }
-
-// console.getElementById('includeSymbols')
-
-
-
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
